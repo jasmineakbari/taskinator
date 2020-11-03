875cad33daf2f5da2920262b443590f94a1fca66
unknown <jasmineakbari@yahoo.com>
2020-11-03 15:47:21 -0500

    created event to prevent page refresh when submitting form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// references button from html
-var buttonEl = document.querySelector("#save-task");
+// references form from html
+var formEl = document.querySelector("#task-form");
 // references ul from html
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Function to add list item
-var createTaskHandler = function() {
-    //clicking this button will add a new list item .createElement
+var createTaskHandler = function(event) {
+    // this will stop page from reloading upon submitting form
+    event.preventDefault();
+    // clicking this button will add a new list item .createElement
     var listItemEl = document.createElement("li");
-    //Setting class name for li with .className
+    // Setting class name for li with .className
     listItemEl.className = "task-item";
     // Adding text to li with .textContent
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = ("A new task has been added.");
     // .appendChild will add li created to end of ul
     tasksToDoEl.appendChild(listItemEl);
 };
-// Upon clicking button perform function
-buttonEl.addEventListener("click", createTaskHandler);
+// Upon submitting or pressing enter on their kb form perform function
+formEl.addEventListener("submit", createTaskHandler);
